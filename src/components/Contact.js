c9bd213aa64226fc8ca3b81f6c9fccae15da68e0
Zach213 <78453852+Zach213@users.noreply.github.com>
2022-01-30 13:40:13 -0500

    change contact msg

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 export default function Contact(){
    return(
    <div className='contact'>
-    <p><i>Contact me @ rustewed@gmail.com</i></p>
+    <p><i>Hey! I'm a junior just wanting to give back. I'd love any feedback! Contact me @ rustewed@gmail.com</i></p>
   </div>
    );
 }
