3529afdb1f3b936d2ca4cece2fd50cc6f39f99ca
Zach213 <78453852+Zach213@users.noreply.github.com>
2022-01-30 13:02:23 -0500

    change author name to scarlet knight

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -15,7 +15,7 @@ const recipes = [
         title: 'Solid Greek Salad',
         image: greeksalad,
         description: 'Tomato slices, Cucumber, Red onion, olives, oil, vinegar',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch',
             'CollegeAve',
@@ -29,7 +29,7 @@ const recipes = [
         title: 'Root Beer Float',
         image: rootbeerfloat,
         description: 'Put vanilla ice cream or vanilla soft serve into the bottom of a dining hall cup. Then take it to the drink station & fill the cup with root beer until you get that nice foam at the top.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch',
             'CollegeAve',
@@ -43,7 +43,7 @@ const recipes = [
         title: 'Protein Toast',
         image: proteintoast,
         description: 'Take a slice of sourdough bread. Toast it lightly. Coat the bread with hummus. Then if Livi has bean salad at the deli bar that day, put bean salad on the toast. Then top with shredded or feta cheese if you like.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Livingston'
         ],
@@ -54,7 +54,7 @@ const recipes = [
         title: 'Trail Mix',
         image: trailmix,
         description: 'Take the nuts, m&m, chocolates chips, crashing or raisins and it I see dried fruits mix together, Ziploc it and take it home.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Livingston'
         ],
@@ -65,7 +65,7 @@ const recipes = [
         title: 'Crunchy Roasted Pepper Panini',
         image: redpanini,
         description: 'Rip a tortilla (by the fruit) in half and spread with hummus (health bar) on one side. Place roasted peppers and tortilla chips (if available at health bar) on the hummus. Add onions and whatever else youd like (salad bar). Put in panini press. Then drizzle chipotle mayo or your condiment of choice on the inside.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Livingston'
         ],
@@ -76,7 +76,7 @@ const recipes = [
         title: 'Peanut butter toast with banana and honey',
         image: pbtoast,
         description: 'During breakfast, take a slice of bread (my favorite is rye!) and toast it. Now take a banana and slice. Assemble like an open face sandwich, putting peanut butter on the bread, then the banana slices, and drizzle honey on top! This can also be done with Nutella and banana.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch',
             'CollegeAve',
@@ -90,7 +90,7 @@ const recipes = [
         title: 'Sad Pasta™ Plus',
         image: sadpasta,
         description: 'On days where theres simply nothing that you like and dont want to wait in line, dont resort to the Sad Pasta™ with the Sad Sauce™. Instead, take the Sad Pasta™ and combine it with olive oil from the salad section (and maybe add some other stuff if youre into that). Afterwards, go ahead and add some cheese or other toppings if you want.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch'
         ],
@@ -101,7 +101,7 @@ const recipes = [
         title: 'Nutella Waffles',
         image: waffle,
         description: 'Find the waffle station, spray iron with non-stick spray, fill cup with waffle batter and pour into iron (You can also fill half the cup and pour into one side if you arent feeling a whole waffle), flip and wait for it to beep.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch',
             'CollegeAve',
@@ -115,7 +115,7 @@ const recipes = [
         title: 'Caesar Meat Salad',
         image: caesarSalad,
         description: 'Take lettuce, cheese, and croutons and sprinkle with Caesar dressing from salad bar, top with meat from sandwhiches at the sandwhich bar.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch',
             'CollegeAve',
@@ -129,7 +129,7 @@ const recipes = [
         title: 'M&M Ice cream sandwhich',
         image: cookiemnm,
         description: 'Take 2 sugar cookies from dessert line, vanilla soft serve between them and drop m&ms on it.',
-        author: 'Zach',
+        author: 'Scarlet Knight',
         location: [
             'Busch',
             'CollegeAve',
