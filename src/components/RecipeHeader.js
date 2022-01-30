0b0640599414f5793822f99cfbc82f91065f2c51
Zach213 <78453852+Zach213@users.noreply.github.com>
2022-01-30 13:18:17 -0500

    style changes

--- src/components/RecipeHeader.js
+++ src/components/RecipeHeader.js
@@ -1,10 +1,14 @@
 import React from 'react'
 export default function RecipeHeader() {
     return (
+        <div>
         <div className='recipeHeader' >
-            <h1 className='recipeListLabel'>
-                <span>Recipe list</span>
-            </h1>
+            <div className='recipeListLabel'>
+            <p>Hey, this is a website for Rutgers students to share dining hall recipes they've made with each other!</p>
+            <p> Hope it's helpful, and drop your own above!</p>
+                <h1 className ='recipeListLabel'>Recipe list</h1>
+            </div>
         </div >
+        </div>
     );
 }
